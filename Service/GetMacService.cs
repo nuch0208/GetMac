cc84af39698e7781a30554621807984bdcdc08ec
nuch0208 <nutchanet2887@gmail.com>
2024-10-07 15:41:10 +0700

    first

---
+++ Service/GetMacService.cs
@@ -0,0 +1,22 @@
+using System.Net.NetworkInformation;
+
+namespace  GetMac.Service
+{
+    public class MacAddress
+    
+    {
+    public string GetMACAddress()
+        {
+            NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            String sMacAddress = string.Empty;
+            foreach (NetworkInterface adapter in nics)
+            {
+                if (sMacAddress == String.Empty)// only return MAC Address from first card  
+                {
+                    IPInterfaceProperties properties = adapter.GetIPProperties();
+                    sMacAddress = adapter.GetPhysicalAddress().ToString();
+                }
+            }return sMacAddress;   
+        }
+    }
+}
